235a7fcde3252ae590105f0b7f1277cce70152bc
Leonardo Almeida <leonardo.er404@gmail.com>
2019-04-11 20:13:18 -0300

    JMetro retirado e SplashScreen recriada

--- src/java/br/edu/ifma/ticketif/Main.java
+++ src/java/br/edu/ifma/ticketif/Main.java
@@ -13,7 +13,6 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Splash.fxml"));
-        new JMetro(JMetro.Style.LIGHT).applyTheme(root);
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
         stage.initStyle(StageStyle.UNDECORATED);
