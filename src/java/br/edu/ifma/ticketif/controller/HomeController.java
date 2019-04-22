169ad5cdddfe1244e19823f968ae5ff1072527fb
Leonardo Almeida <leonardo.er404@gmail.com>
2019-04-22 10:41:16 -0300

    Dias da Semana em Menu Home

--- src/java/br/edu/ifma/ticketif/controller/HomeController.java
+++ src/java/br/edu/ifma/ticketif/controller/HomeController.java
@@ -9,16 +9,22 @@ import javafx.scene.layout.BorderPane;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.ResourceBundle;
 
 
 public class HomeController implements Initializable {
 
+    GregorianCalendar gc = new GregorianCalendar();
     DateTimeFormatter dateformater = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     LocalDate localDate = LocalDate.now();
 
     @FXML
     private Label label_dataatual;
+
+    @FXML
+    private Label diasemana;
 
     @FXML
     private BorderPane telaHome;
@@ -29,6 +35,43 @@ public class HomeController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         label_dataatual.setText(dateformater.format(localDate));
         System.out.println("Data de conexão: " + dateformater.format(localDate));
+
+        switch (gc.get(Calendar.DAY_OF_WEEK)){
+
+            case Calendar.SUNDAY:
+                diasemana.setText("Domingo");
+             break;
+
+            case Calendar.MONDAY:
+                diasemana.setText("Segunda-Feira");
+            break;
+
+            case Calendar.TUESDAY:
+                diasemana.setText("Terça-Feira");
+                break;
+
+            case Calendar.WEDNESDAY:
+                diasemana.setText("Quarta-Feira");
+                break;
+
+            case Calendar.THURSDAY:
+                diasemana.setText("Quinta-Feira");
+                break;
+
+            case Calendar.FRIDAY:
+                diasemana.setText("Sexta-Feira");
+                break;
+
+            case Calendar.SATURDAY:
+                diasemana.setText("Sabado");
+                break;
+
+            default:
+                System.out.println("Nada");
+
+
+        }
+
         btnCadastrar();
 
     }
